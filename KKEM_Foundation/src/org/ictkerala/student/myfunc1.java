ead1fcdba3df5c7cc267598647f81c7bfe07cdce
KP <KP@DESKTOP-781QH81>
2022-08-09 16:07:52 +0530

    my second attempt

---
+++ KKEM_Foundation/src/org/ictkerala/student/myfunc1.java
@@ -0,0 +1,14 @@
+package org.ictkerala.student;
+
+public class myfunc1 {
+
+		// TODO Auto-generated method stub
+       int prodout(int x, int y)
+       {
+			return(x*y);
+		}
+       int addout(int x,int y)
+       {
+    	   return(x+y);
+       }
+	                  }
